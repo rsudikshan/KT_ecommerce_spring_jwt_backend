2a8aa6b0c66b01763ff383383ed78de1497835f7
Sudikshan <rsudikshan2@gmail.com>
2024-12-12 11:51:20 +0545

    first commit

---
+++ src/main/java/com/sr/KT_ecommerce_spring_jwt_backend/KtEcommerceSpringJwtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sr.KT_ecommerce_spring_jwt_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtEcommerceSpringJwtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtEcommerceSpringJwtBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sr/KT_ecommerce_spring_jwt_backend/KtEcommerceSpringJwtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sr.KT_ecommerce_spring_jwt_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtEcommerceSpringJwtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
